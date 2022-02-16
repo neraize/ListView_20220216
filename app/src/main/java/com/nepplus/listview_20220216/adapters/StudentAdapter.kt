bcfa357a8c3482c6a85408aae529730c9eadff72
seojungmin <neraize@gmail.com>
2022-02-16 16:16:54 +0900

    실제데이터 반영  -사용할 데이터를 mList[position]로 가져오기  -뿌려줄 UI들을, row.findViewById로 가져오기  -상황에 따른 데이터 표시하기

--- app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.nepplus.listview_20220216.R
 import com.nepplus.listview_20220216.datas.StudentData
 
@@ -22,6 +23,17 @@ class StudentAdapter(
             tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
         }
         val row = tempRow!!
+
+        val data = mList[position]
+        val txtStudentName = row.findViewById<TextView>(R.id.txtStudentName)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+        val txtAddress = row.findViewById<TextView>(R.id.txtAddress)
+
+        txtStudentName.text =data.name
+        txtAddress.text =data.address
+
+        val age = 2022 -data.birthYear +1
+        txtAge.text ="(${age})"
 
         return row
     }
