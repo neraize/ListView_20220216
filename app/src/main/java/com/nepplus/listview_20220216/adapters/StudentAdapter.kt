2c537750143d125995f96ffb6a0228651c6507bd
seojungmin <neraize@gmail.com>
2022-02-16 15:36:19 +0900

    tempRow를 검사해서, null이라면  - 새로 xml을 끌고와서 대입시키기

--- app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.nepplus.listview_20220216.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.listview_20220216.R
 import com.nepplus.listview_20220216.datas.StudentData
 
 class StudentAdapter(
@@ -10,4 +14,11 @@ class StudentAdapter(
     val mList:ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList){
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow==null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
+        }
+    }
 }
