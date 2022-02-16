ac0825ff1cef95e7d49ae5adc4644a2f36d2dff9
seojungmin <neraize@gmail.com>
2022-02-16 16:36:54 +0900

    리스트뷰의 각 줄이 눌렸을때에 대한 이벤트 처리

--- app/src/main/java/com/nepplus/listview_20220216/MainActivity.kt
+++ app/src/main/java/com/nepplus/listview_20220216/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.listview_20220216
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.nepplus.listview_20220216.adapters.StudentAdapter
 import com.nepplus.listview_20220216.datas.StudentData
 import kotlinx.android.synthetic.main.activity_main.*
@@ -30,5 +31,11 @@ class MainActivity : AppCompatActivity() {
         mAdapter =  StudentAdapter(this, R.layout.student_list_item, mStudentList)
 
         studentListView.adapter = mAdapter
+        
+        studentListView.setOnItemClickListener { adapterView, view, position, l ->
+
+            val clickedStudent = mStudentList[position]
+            Toast.makeText(this, "${clickedStudent.name} 학생 클릭됨", Toast.LENGTH_SHORT).show()
+        }
     }
 }
